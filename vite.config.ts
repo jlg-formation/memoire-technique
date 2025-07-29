158da351bafe37f8a8415af9efe9d6da30878d20
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-29 12:09:04 +0200

    feat: add GitHub Pages deployment workflow and custom 404 redirection page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/memoire-technique/", // ← Ajouté pour GitHub Pages
   plugins: [react(), tailwindcss()],
 });
